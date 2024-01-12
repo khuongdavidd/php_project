17e1a68d07691eb7d43a2cbe41492b54d5db9a75
khuongdavid <khuongdavid111@gmail.com>
2024-01-12 16:00:45 +0700

    change in config.php 12/1/2024 by nguyen vu khuong

--- config.php
+++ config.php
@@ -4,8 +4,8 @@
     Người viết: Nguyễn Vũ Khương
     Ngày: 02/01/2024
     */
-    // define('DB_HOST','localhost');
-    // define('DB_NAME','db_ct06');
-    // define('DB_USER','ad_db_ct06');
-    // define('DB_PASS','dbct06');
+    define('DB_HOST','localhost');
+    define('DB_NAME','db_ct06');
+    define('DB_USER','ad_db_ct06');
+    define('DB_PASS','dbct06');
 ?>
